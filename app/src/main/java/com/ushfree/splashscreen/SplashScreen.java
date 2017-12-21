e8ba93916b7f5745e41eb782baaa7e2dc591b0d9
UsamahZundulloh <usamah.jundulloh10@gmail.com>
2017-12-21 07:49:25 +0700

    SplashScreen

---
+++ app/src/main/java/com/ushfree/splashscreen/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.ushfree.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMER = 5000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish();
+            }
+        }, SPLASH_TIMER);
+    }
+}
